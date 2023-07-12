893781b8aa1eed8249ab4f2a2f789eabe7218f90
Matt Black <mblackonline.git@gmail.com>
2023-07-11 22:12:17 -0400

    feat(css, js): added css and javascript for word "Meep".
    The changes include:
    - Added css to change the color of the word "Meep" to red when the user hovers over it.
    - Added event listeners to give some animation when clicking on the word "Meep".
    - Also added some comments to explain what I did.
    
    Signed-off-by: Matt Black

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+// This JavaScript file defines the behavior of the web page.
 
+
+// This variable stores the div element with the class "meep".
+const meep = document.querySelector('.meep');
+
+// This event listener is called when the user clicks on the word "Meep".
+meep.addEventListener('mousedown', () => {
+  // This increases the font size of the word "Meep" to 2em.
+  meep.style.fontSize = '2em';
+});
+
+// This event listener is called when the user releases the button after clicking on the word "Meep".
+meep.addEventListener('mouseup', () => {
+  // This resets the font size of the word "Meep" to 1em.
+  meep.style.fontSize = '1em';
+});
