1265fe9ded24016f8a6346a8a78b7f372e9ac46f
WonderboyPark <stevenpark84@gmail.com>
2023-10-14 14:37:56 -0400

    update to mobile shrinking header
    got shrinking header to work
    not sure if its the right way... might need some refactoring

--- index.js
+++ index.js
@@ -27,18 +27,31 @@ const dialog = document.getElementById("learnMore");
 //   dialog.close("");
 // });
 
-const contentWrapper = document.querySelector(".mainContentWrapper")
-
-
-const sidebar = document.querySelector(".sidebar")
+let contentWrapper = document.querySelector(".content")
+let contentWrapperPostion = contentWrapper.scrollTop;
+const mainContentWrapper = document.querySelector(".mainContentWrapper")
+const sidebarWrapper = document.querySelector(".sidebarWrapper")
 const logo = document.querySelector(".horizontal-logo")
+const socials = document.querySelector(".socials-list")
+const viewportWidth = window.innerWidth;
 
+// console.log(viewportWidth)
 
+if(viewportWidth <= 768) {
+  contentWrapper.addEventListener("scroll", (event) => {
+    let contentContainerScroll = contentWrapper.scrollTop;
+    if(contentContainerScroll > contentWrapperPostion) {
+      // console.log("hello")
+      logo.classList.add("small-horizontal-logo");
+      socials.classList.add("socials-list-hidden")
+      sidebarWrapper.classList.add("shortSidebarWrapper")
+      mainContentWrapper.classList.add("shortMainContentWrapper")
+    } else {
+      logo.classList.remove("small-horizontal-logo");
+      socials.classList.remove("socials-list-hidden")
+      sidebarWrapper.classList.remove("shortSidebarWrapper")
+      mainContentWrapper.classList.remove("shortMainContentWrapper")
+    }
+  })
+}
 
-contentWrapper.addEventListener("scroll", () => {
-  const contentContainer = document.querySelector(".content")
-  let contentContainerScroll = contentContainer.scrollTop;
-  if(contentContainerScroll > contentWrapper) {
-    console.log("hello")
-  }
-})
